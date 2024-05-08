cfb8ecba6403a462b0be1271a6b2128abdc92f25
qiyangku <yaqi4276@uni.sydney.edu.au>
2024-05-09 06:42:31 +0800

    add batch dimension and config

--- projects/dtb/mnn_dtb.py
+++ projects/dtb/mnn_dtb.py
@@ -8,21 +8,17 @@ Created on Thu May  2 20:54:44 2024
 import torch
 import numpy as np
 from projects.dtb.ma_conductance import Moment_Activation_Cond
+from projects.dtb.get_connectivity import calculate_degree
+from matplotlib import pyplot as plt
 
 
 class Cond_MNN_DTB():
-    def __init__(self, device='cpu'):
+    def __init__(self, config, device='cpu'):
         self.device = device
         # initialize activation functions
-        self.exc_activation = Moment_Activation_Cond() 
+        self.exc_activation = Moment_Activation_Cond()
         self.inh_activation = Moment_Activation_Cond()
-        
-        # TODO: update neuronal parameters, which are different for E/I pops 
-        # self.exc_activation
-        
-        # TODO: inter-regional synapses have its own conductance
-        # which should be treated as a different channel.
-        
+                
         # initializing DTI-constrained connectivity in-degree 
         K_EE, K_EI, K_IE, K_II, K_EE_long = calculate_degree()
         self.N = K_EE.shape[0] # number of brain regions
@@ -36,27 +32,20 @@ class Cond_MNN_DTB():
         self.dt_mnn = 0.1 #simulation time step for MNN
         
         # calculate background current stats
-        Le = 0.05 # leak conductance of exc neuron; unit in micro S
-        Li = 0.1 # leak conductance of inh neuron
-        self.bg_mean_to_exc = 0.36/Le #nA/uS; output is mV; 50 leak conductance in nS
-        self.bg_mean_to_inh = 0.54/Li
-        self.bg_std_to_exc = 1.0/Le
-        self.bg_std_to_inh = 1.2/Li
+        self.bg_mean_to_exc = config['bg_mean_to_exc'] #nA/uS; output is mV
+        self.bg_mean_to_inh = config['bg_mean_to_inh'] 
+        self.bg_std_to_exc = config['bg_std_to_exc'] 
+        self.bg_std_to_inh = config['bg_std_to_inh'] 
         
-        self.xregion_gain = 0.0 # cross regional exc connection gain
+        self.xregion_gain = config['xregion_gain'] # cross regional exc connection gain
+        self.batchsize = config['batchsize']
         
         
     def forward(self, ue, se, ui, si):
         '''
-        ue, se: mean/std of excitatory neurons  dims: batch x N
+        ue, se: mean/std of excitatory neurons  dims: batchsize x num of neurons
         ui, si: mean/std of inhibitory neurons
         '''
-        
-        # TODO: long range excitation should be a separate channel
-        
-        # STEP 1: excitatory neurons
-        # inputs to excitatory population
-        # TODO: no need a new channel for long-range EE, just add the scaling factor here
         
         exc_input_mean = torch.mm(ue, self.K_EE) + self.xregion_gain*torch.mm( ue, self.K_EE_long.T )
         inh_input_mean = torch.mm(ui, self.K_EI)
@@ -64,6 +53,7 @@ class Cond_MNN_DTB():
         exc_input_std = exc_input_std.pow(0.5)
         inh_input_std = torch.mm(si*si, self.K_EI).pow(0.5)
         
+        # excitatory population
         # convert conductance input to current
         eff_input_mean, eff_input_std, tau_eff = self.exc_activation.cond2curr(exc_input_mean, exc_input_std, inh_input_mean, inh_input_std)
         # add background current
@@ -73,9 +63,7 @@ class Cond_MNN_DTB():
         # calculate moment activation 
         exc_mean_out, exc_std_out = self.exc_activation.activate( eff_input_mean, eff_input_std, tau_eff)
         
-        
-        # STEP 2: inhibitory neurons
-        # a better way is to recycle the code?
+        # inhibitory population
         exc_input_mean = torch.mm(ue, self.K_IE)
         inh_input_mean = torch.mm(ui, self.K_II)
         exc_input_std = torch.mm(se*se, self.K_IE).pow(0.5)
@@ -106,25 +94,49 @@ class Cond_MNN_DTB():
             #print(i)
             exc_mean_out, exc_std_out, inh_mean_out, inh_std_out = self.forward(ue,se,ui,si)
             
-            ue += self.dt_mnn*(-ue + exc_mean_out)
-            se += self.dt_mnn*(-se + exc_std_out)
+            ue = ue+ self.dt_mnn*(-ue + exc_mean_out)
+            se = se+ self.dt_mnn*(-se + exc_std_out)
             
-            ui += self.dt_mnn*(-ui + inh_mean_out)
-            si += self.dt_mnn*(-si + inh_std_out)
+            ui = ui+ self.dt_mnn*(-ui + inh_mean_out)
+            si = si+ self.dt_mnn*(-si + inh_std_out)
             
         return ue, se, ui, si
         
 if __name__=='__main__':
+    torch.set_default_dtype(torch.float64)
+    
     device='cpu'
+    batchsize = 21
+    config ={
+        'batchsize': batchsize,
+        'bg_mean_to_exc': 0.4, #nA/uS; output is mV; 50 leak conductance in nS
+        'bg_mean_to_inh': 0.13,
+        'bg_std_to_exc': 0.0,
+        'bg_std_to_inh': 0.0,
+        'xregion_gain': torch.linspace(0,2,batchsize, device=device).unsqueeze(1) # shape is batchsize x 1
+        }
+        #Le = 0.05 # leak conductance of exc neuron; unit in micro S
+        #Li = 0.1 # leak conductance of inh neuron
     T = 10
     
-    mnn = Cond_MNN_DTB(device=device)
+    mnn = Cond_MNN_DTB(config, device=device)
     ue, se, ui, si = mnn.run(T=T)
-    print('Done!')
     
     plt.close('all')
-    plt.plot(ue.flatten(),'.')
-    plt.plot(ui.flatten(),'.')
+    
+    plt.plot(ue.flatten().cpu().numpy())
+    
+    
+    # extent = (config['xregion_gain'][0,0],config['xregion_gain'][-1,0], 1, ue.shape[1])
+    
+    # plt.figure(figsize=(3.5,3))
+    # plt.imshow(ue.T.cpu().numpy().round(3), aspect='auto', origin='lower', extent=extent)
+    # plt.ylabel('Region index')
+    # plt.xlabel('Cross-region gain')
+    # plt.title('Mean firing rate (sp/ms)')
+    # plt.colorbar()
+    # plt.tight_layout()
+    
     
     
         
