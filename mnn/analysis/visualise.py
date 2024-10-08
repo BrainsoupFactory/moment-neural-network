df3a867066e48f586a9ffcf56e4cea4ab0178c6c
zhichaozhu <zachary_zhu@outlook.com>
2024-10-08 18:08:41 +0800

    add "plot_ellipse" function

--- mnn/analysis/visualise.py
+++ mnn/analysis/visualise.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import os
-
+import torch
+from torch import Tensor
+from typing import Tuple, Optional
 
 def rename_duplicate_file(file_path, file_name, suffix_pos=None):
     temp = file_name
@@ -48,3 +50,23 @@ def prepare_fig_axs(nrows: int, ncols: int, flatten=True, **kwargs):
     if flatten and nrows * ncols > 1:
         axs = axs.reshape(-1)
     return fig, axs
+
+def normalize_mean_cov(u: Tensor, cov: Tensor,  eps: float = 1e-6) -> Tuple[Tensor, Tensor]:
+    weight = torch.sum(torch.pow(u, 2), dim=-1, keepdim=True) + eps
+    cov = cov / weight.unsqueeze(-1)
+    u = u / torch.sqrt(weight)
+    return u, cov
+
+def plot_ellipse(ax, u, cov, normalize=False, scale=1, n_std=1, **kwargs):
+    if normalize:
+        u, cov = normalize_mean_cov(u, cov, eps=1e-8)
+    u = u * scale
+    cov = cov * scale
+    eig_value, eig_vector = torch.linalg.eigh(cov)
+    th = torch.linspace(0, 2 * np.pi, 101)
+    x = torch.sqrt(eig_value[0]) * torch.cos(th) * n_std
+    y = torch.sqrt(eig_value[1]) * torch.sin(th) * n_std
+    r = torch.cat([x.unsqueeze(-1), y.unsqueeze(-1)], dim=-1)
+    r = torch.matmul(eig_vector, r.unsqueeze(-1)).squeeze(-1)
+    ax.plot(r[:, 0] + u[0], r[:, 1] + u[1], **kwargs)
+    return ax
